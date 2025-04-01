3d96bb24957de0be427f3e2def171486463d3b64
Gurova Mariia (gurovmar) <gurovmar@students.zhaw.ch>
2025-04-02 01:22:29 +0200

    comments

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import pytest
 from app import app
 
 
-@pytest.fixture
+@pytest.fixture   # -> Sets up a test client to simulate HTTP requests
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 
-def test_app_is_working(client):
+def test_app_is_working(client): # -> send a GET request to the root /
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 200 # -> (status code OK)
     assert b"Hello from version 2." in response.data
